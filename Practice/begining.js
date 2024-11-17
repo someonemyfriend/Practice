47eda794b095d28daae3aefb2595ef92ba72eb21
Vasilia Staykova <someonemyfriend@gmail.com>
2024-11-17 16:38:37 +0200

    Add files via upload

---
+++ Practice/begining.js
@@ -0,0 +1,120 @@
+// 01.--------------------------------------------------
+
+// function num1to10() {
+//     console.log(1);
+//     console.log(2);
+//     console.log(3);
+//     console.log(4);
+//     console.log(5);
+//     console.log(6);
+//     console.log(7);
+//     console.log(8);
+//     console.log(9);
+//     console.log(10);
+// }
+
+// num1to10();
+
+// 02.--------------------------------------------------
+
+// function hello() {
+//     console.log("Hello SoftUni");
+// }
+// hello();
+
+// 03.--------------------------------------------------
+
+// function squareArea(a) {
+//     let area = a * a;
+//     console.log(area);
+// }
+
+// squareArea(5);
+
+// 04.--------------------------------------------------
+
+// function inches(sm) {
+//     let inches = sm * 2.54;
+//     console.log(inches);
+// }
+
+// inches(7);
+
+// 05.--------------------------------------------------
+
+// function greetings(name) {
+//     console.log(`Hello, ${name}!`);
+// }
+
+// greetings("Niki");
+
+// 06.--------------------------------------------------
+
+// function concatenateData(firstName, lastName, age, town) {
+//     console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`);
+// }
+
+// concatenateData("Maria", "Ivanova", 20, "Sofia");
+
+// 07.--------------------------------------------------
+
+// function project(name, projects) {
+
+//     let countOfHours = projects * 3;
+//     console.log(`The architect ${name} will need ${countOfHours} hours to complete ${projects} project/s.`);
+// }
+
+// project("Georgi", 4)
+
+// 08.--------------------------------------------------
+
+// function food(dogs, cats) {
+
+//     let priceForDogs = dogs * 2.50;
+//     let priceForCats = cats * 4;
+//     let sum = priceForDogs + priceForCats;
+//     console.log(sum + " lv.");
+// }
+
+// food(5, 4);
+
+// 09.--------------------------------------------------
+
+function greening(kvadratniMetri) {
+
+    let greetingAll = kvadratniMetri * 7.61;
+    let discount = greetingAll * 0.18;
+    let calculate = greetingAll - discount;
+    console.log(`The final price is: ${calculate} lv.`);
+    console.log(`The discount is: ${discount} lv.`);
+}
+
+greening(550);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
