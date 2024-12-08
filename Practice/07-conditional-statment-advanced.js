49ee016a0305c31b31c5c96c37d50d8f260177d5
Vasilia Staykova <someonemyfriend@gmail.com>
2024-12-08 03:49:29 +0200

    Add files via upload

---
+++ Practice/07-conditional-statment-advanced.js
@@ -0,0 +1,94 @@
+// 01.--------------------------------------------------
+// function dayOfWeek(num) {
+
+//     switch (num) {
+//         case 1:
+//             console.log("Monday");
+//             break;
+//         case 2:
+//             console.log("Tuesday");
+//             break;
+//         case 3:
+//             console.log("Wednesday");
+//             break;
+//         case 4:
+//             console.log("Thursday");
+//             break;
+//         case 5:
+//             console.log("Friday");
+//             break;
+//         case 6:
+//             console.log("Saturday");
+//             break;
+//         case 7:
+//             console.log("Sunday");
+//             break;
+//         default:
+//             console.log("Error");
+//             break;
+//     }
+// }
+// dayOfWeek(1);
+
+// 02.--------------------------------------------------
+
+// function days(day) {
+//     switch (day) {
+//         case "Monday":
+//         case "Tuesday":
+//         case "Wednesday":
+//         case "Thursday":
+//         case "Friday":
+//             console.log("Working day");
+//             break;
+//         case "Saturday":
+//         case "Sunday":
+//             console.log("Weekend");
+//             break;
+//         default:
+//             console.log("Error");
+//             break;
+//     }
+// }
+// days("Monday");
+
+// 03.--------------------------------------------------
+
+function days(animal) {
+    switch (animal) {
+        case "dog":
+            console.log("mammal");
+            break;
+        case "crocodile":
+        case "tortoise":
+        case "snake":
+            console.log("reptile");
+            break;
+        default:
+            console.log("unknown");
+            break;
+    }
+}
+days("dog");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
