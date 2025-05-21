df2db80215fdb7f11fb4ca2a3ec7df029987b1c1
Vasilia Staykova <someonemyfriend@gmail.com>
2025-05-21 18:26:34 +0300

    added task1

--- jsAdPractice/ExeSyntaxFunctionsStatements.js
+++ jsAdPractice/ExeSyntaxFunctionsStatements.js
@@ -1,2 +1,11 @@
 //=====================================================
 //  01.
+
+function fruit(str, grams, price){
+
+    let gramToKilo = (grams * 0.001).toFixed(2);
+    let calcPrice = (gramToKilo * price).toFixed(2);
+    console.log(`I need $` + `${calcPrice} to buy ${gramToKilo} kilograms ${str}.`);
+}
+
+fruit('apple', 1563, 2.35);
