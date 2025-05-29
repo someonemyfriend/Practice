3bec59ee5e1e3c3bc4cb72a01279b8215ff5cb08
Vasilia Staykova <someonemyfriend@gmail.com>
2025-05-29 21:21:42 +0300

    add 2 task

--- jsAdPractice/ExeSyntaxFunctionsStatements.js
+++ jsAdPractice/ExeSyntaxFunctionsStatements.js
@@ -1,11 +1,67 @@
 //=====================================================
 //  01.
 
-function fruit(str, grams, price){
+// function fruit(str, grams, price){
 
-    let gramToKilo = (grams * 0.001).toFixed(2);
-    let calcPrice = (gramToKilo * price).toFixed(2);
-    console.log(`I need $` + `${calcPrice} to buy ${gramToKilo} kilograms ${str}.`);
+//     let gramToKilo = (grams * 0.001).toFixed(2);
+//     let calcPrice = (gramToKilo * price).toFixed(2);
+//     console.log(`I need $` + `${calcPrice} to buy ${gramToKilo} kilograms ${str}.`);
+// }
+
+// fruit('apple', 1563, 2.35);
+
+//=====================================================
+//  02
+
+// function solve(a, b) {
+//     let solve = a % b;
+//     while(solve !==  0) {
+//         a = b;
+//         b = solve;
+//         solve= a % b;
+//     }
+
+//     console.log(b);
+// }
+
+// solve(15, 5);
+// solve(2154, 458);
+
+//=====================================================
+//  03
+
+function solve(num) {
+    let numAsString = num.toString();
+
+    let firstElement = Number(numAsString[0]);
+    let sum = firstElement;
+    let isSame = true;
+
+    for(let i = 1; i < numAsString.lenght; i++){
+        let currentNum = Number(numAsString[i]);
+        
+        if (let i=1; i<numAsString)
+    }
+
+
+
 }
+solve(222222);
 
-fruit('apple', 1563, 2.35);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
